6cc38600710f32a55c352991ed90434d335d6844
Ralf Lang <lang@b1-systems.de>
2017-10-22 11:49:15 +0000

    Add a Dummy authentication driver and unit tests

---
+++ lib/Horde/Auth/Dummy.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Copyright 2017-2018 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you did
+ * not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @author   Ralf Lang <lang@b1-systems.de>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/lgpl21 LGPL-2.1
+ * @package  Auth
+ */
+
+/**
+ * The Horde_Auth_Dummy class provides an in-memory user list.
+ * It is meant to be used for throwaway setups, satellite systems or for
+ * providing a source of administrative accounts in Composite or Cascading driver
+ * The driver can also be used as a mock-like backend for integration tests
+ *
+ * @author    Ralf Lang <lang@b1-systems.de>
+ * @category  Horde
+ * @copyright 2017-2018 Horde LLC
+ * @license   http://www.horde.org/licenses/lgpl21 LGPL-2.1
+ * @package   Auth
+ */
+class Horde_Auth_Dummy extends Horde_Auth_Base
+{
+    /**
+     * An array of capabilities, so that the driver can report which
+     * operations it supports and which it doesn't.
+     *
+     * @var array
+     */
+    protected $_capabilities = array(
+        'authenticate' => true,
+        'list' => true,
+        'update' => true,
+        'remove' => true,
+        'add' => true,
+    );
+
+    /**
+     * Constructor.
+     *
+     * @param array $params  Optional parameters:
+     * <pre>
+     * 'users' - (array) Usernames are hash keys, passwords are values
+     * 'encryption' - (string) Optionally supply an encryption or hashing
+     * 'show_encryption' - (boolean) prepend encryption info to password string
+     * </pre>
+     */
+    public function __construct(array $params = array())
+    {
+        $params = array_merge(
+            array(
+                'encryption' => 'plain',
+                'users' => array(),
+                'show_encryption' => false
+            ),
+            $params
+        );
+        parent::__construct($params);
+    }
+
+    /**
+     * Adds a set of authentication credentials.
+     *
+     * @param string $userId      The userId to add.
+     * @param array $credentials  The credentials to use.
+     *
+     * @throws Horde_Auth_Exception
+     */
+    public function addUser($userId, $credentials)
+    {
+        // TODO: use persistence callback if given
+        if ($this->exists($userId)) {
+            throw new Horde_Auth_Exception('User already exists');
+        }
+        $this->_params['users'][$userId] = Horde_Auth::getCryptedPassword(
+               $credentials['password'],
+               '',
+               $this->_params['encryption'],
+               $this->_params['show_encryption']);
+    }
+
+    /**
+     * Lists all users in the system.
+     *
+     * @param boolean $sort  Sort the users?
+     *
+     * @return mixed  The array of userIds.
+     * @throws Horde_Auth_Exception
+     */
+    public function listUsers($sort = false)
+    {
+        return $this->_sort(array_keys($this->_params['users']), $sort);
+    }
+
+    /**
+     * Updates a set of authentication credentials for the life time of the driver.
+     *
+     * @param string $oldID       The old userId.
+     * @param string $newID       The new userId.
+     * @param array $credentials  The new credentials
+     *
+     * @throws Horde_Auth_Exception
+     */
+    public function updateUser($oldID, $newID, $credentials)
+    {
+        if (!$this->exists($oldID)) {
+            throw new Horde_Auth_Exception('User does not exist');
+        }
+        if ($this->exists($newID) && $newID != $oldID) {
+            throw new Horde_Auth_Exception('Cannot rename to existing user name');
+        }
+        $this->removeUser($oldID);
+        $this->addUser($newID, $credentials);
+    }
+
+    /**
+     * Deletes a set of authentication credentials for the life of the driver.
+     *
+     * @param string $userId  The userId to delete.
+     *
+     * @throws Horde_Auth_Exception
+     */
+    public function removeUser($userId)
+    {
+        // TODO: use persistence callback if given
+        unset($this->_params['users'][$userId]);
+    }
+
+    /**
+     * Authenticate.
+     *
+     * @param string $userId      The userID to check.
+     * @param array $credentials  An array of login credentials.
+     *
+     * @throws Horde_Auth_Exception
+     */
+    protected function _authenticate($userId, $credentials)
+    {
+        if ($this->exists($userId)) {
+            if ($this->_comparePasswords(
+                $this->_params['users'][$userId],
+                $credentials['password']
+            )) {
+                return;
+            }
+        }
+        throw new Horde_Auth_Exception('', Horde_Auth::REASON_BADLOGIN);
+    }
+
+    /**
+     * Compare an encrypted password to a plaintext string to see if
+     * they match.
+     *
+     * @param string $encrypted  The crypted password to compare against.
+     * @param string $plaintext  The plaintext password to verify.
+     *
+     * @return boolean  True if matched, false otherwise.
+     */
+    protected function _comparePasswords($encrypted, $plaintext)
+    {
+        return $encrypted == Horde_Auth::getCryptedPassword($plaintext,
+                                                       $encrypted,
+                                                       $this->_params['encryption'],
+                                                       $this->_params['show_encryption']);
+    }
+
+}
---
+++ test/Horde/Auth/Unit/DummyCryptTest.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * PHP version 5
+ * Test the Horde_Auth_Dummy:: class.
+ *
+ * Copyright 2017-2018 Horde LLC (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.horde.org/licenses/lgpl21.
+ *
+ * @category   Horde
+ * @package    Auth
+ * @subpackage UnitTests
+ * @author     Ralf Lang <lang@b1-systems.de>
+ * @license    http://www.horde.org/licenses/lgpl21 LGPL-2.1
+ * @link       http://pear.horde.org/index.php?package=Auth
+ */
+class Horde_Auth_Unit_DummyCryptTest extends Horde_Auth_TestCase
+{
+    public function setUp()
+    {
+        $this->driver = new Horde_Auth_Dummy(
+            array(
+                'users' => array(
+                    'user1' => '$1$S/EKq8Dg$OJvaV8Lu1HgCXKNqAo.wG/',
+                    'user2' => '$1$aCWZiBAW$dK0DCmTGYR1gEX11pMxbi0',
+                    'tester' => '$1$sjSx+Q9x$3WIEdh1Ei16QouYx1Xkct1'
+                ),
+                'encryption' => 'crypt-md5',
+                'show_encryption' => false
+           )
+        );
+    }
+
+    public function testAuthenticate()
+    {
+        $this->assertTrue($this->driver->authenticate('user1', array('password' => 'user1pw')));
+        $this->assertTrue($this->driver->authenticate('user2', array('password' => 'user2pw')));
+        $this->assertTrue($this->driver->authenticate('tester', array('password' => 'WeirdPW92401#1')));
+        // correct password extended by garbage
+        $this->assertFalse($this->driver->authenticate('user1', array('password' => 'user1pwfalse')));
+        // Existing user with all kinds of garbage
+        $this->assertFalse($this->driver->authenticate('user1', array('password' => 'any')));
+        $this->assertFalse($this->driver->authenticate('user1', array('password' => '')));
+        $this->assertFalse($this->driver->authenticate('user1', array('password' => null)));
+        $this->assertFalse($this->driver->authenticate('user1', array('password' => '0')));
+        // Unknown user with all kinds of garbage
+        $this->assertFalse($this->driver->authenticate('unknownuser', array('password' => 'any')));
+        $this->assertFalse($this->driver->authenticate('unknownuser', array('password' => '')));
+        $this->assertFalse($this->driver->authenticate('unknownuser', array('password' => null)));
+        $this->assertFalse($this->driver->authenticate('unknownuser', array('password' => '0')));
+    }
+
+    public function testRemoveUser()
+    {
+        // Delete somebody who doesn't exist
+        $this->driver->removeUser('user');
+        $this->assertCount(3, $this->driver->listUsers());
+        $this->driver->removeUser('user1');
+        $this->assertCount(2, $this->driver->listUsers());
+        $this->driver->removeUser('user2');
+        $this->assertCount(1, $this->driver->listUsers());
+        $this->driver->removeUser('tester');
+        $this->assertCount(0, $this->driver->listUsers());
+        // Restore setup
+        $this->setUp();
+    }
+
+    public function testAddUser()
+    {
+        // Add somebody who already exist
+        $this->driver->addUser('user4', array('password' => 'foo'));
+        $this->assertCount(4, $this->driver->listUsers());
+        // Add somebody who already exist
+        $this->setExpectedException(Horde_Auth_Exception::class);
+        $this->driver->addUser('user4', array('password' => 'foo'));
+        $this->assertCount(4, $this->driver->listUsers());
+
+        // Restore setup
+        $this->setUp();
+    }
+
+    public function testUpdateUser()
+    {
+        // Try a password change
+        $this->driver->updateUser('tester', 'tester',  array('password' => 'foo'));
+        $this->assertCount(3, $this->driver->listUsers());
+        $this->assertTrue($this->driver->exists('tester'));
+        // Try renaming
+        $this->driver->updateUser('tester', 'newname',  array('password' => 'foo'));
+        $this->assertCount(3, $this->driver->listUsers());
+        $this->assertTrue($this->driver->exists('newname'));
+        $this->assertFalse($this->driver->exists('tester'));
+        $this->setUp();
+    }
+
+    public function testUpdateUserFailDoesNotExist()
+    {
+        // Try renaming unknown user
+        $this->setExpectedException(Horde_Auth_Exception::class);
+        $this->driver->updateUser('unknownuser', 'newname',  array('password' => 'foo'));
+        $this->setUp();
+    }
+
+    public function testUpdateUserFailNewNameExists()
+    {
+        // Try renaming unknown user
+        $this->setExpectedException(Horde_Auth_Exception::class);
+        $this->driver->updateUser('tester', 'user1',  array('password' => 'foo'));
+        $this->setUp();
+    }
+
+
+
+    public function testExists()
+    {
+        $this->assertTrue($this->driver->exists('user1'));
+        $this->assertTrue($this->driver->exists('user2'));
+        $this->assertTrue($this->driver->exists('tester'));
+        $this->assertFalse($this->driver->exists('somebody'));
+        $this->assertFalse($this->driver->exists(''));
+        $this->assertFalse($this->driver->exists(null));
+    }
+
+    /**
+     * This is actually a test against Horde_Auth_Base
+     * TODO: Copy or move to a test with a phpunit mock
+     */
+
+    public function testSearchUsers()
+    {
+        $this->assertCount(2, $this->driver->searchUsers('user'));
+        $this->assertCount(1, $this->driver->searchUsers('test'));
+        $this->assertEquals(array('tester'), $this->driver->searchUsers('test'));
+    }
+
+    public function testListUsers()
+    {
+        $this->assertEquals(array('tester', 'user1', 'user2'), $this->driver->listUsers(true));
+        $this->assertEquals(array('user1', 'user2', 'tester'), $this->driver->listUsers(false));
+        $this->assertEquals(array('user1', 'user2', 'tester'), $this->driver->listUsers());
+    }
+}
